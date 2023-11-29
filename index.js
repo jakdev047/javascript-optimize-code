42120a6468afc29e1bfe1f3dae034419d918a80e
jakdev047 <jakfreelancer047@gmail.com>
2023-11-30 00:04:02 +0600

    promise add

--- index.js
+++ index.js
@@ -21,3 +21,16 @@ class Person {
 const person = new Person(name);
 
 person.sayHello();
+
+// 4. let's create a promise
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved");
+  }, 1000);
+});
+
+promise.then((result) => {
+  console.log(result);
+});
+
+
