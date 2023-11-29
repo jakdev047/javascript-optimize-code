4a4742126f452e8db28f72d454c16be7f0cadce3
MdSaymun <saymun2000@gmail.com>
2023-11-29 23:51:07 +0600

    new branch created and console log

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const isRequired = () => {
-  throw Error("Argument is required");
-};
+// let's do some interesting stuff with JS 
 
-const setCurrentVideoCode = (videoCode = isRequired()) => {
-  console.log(videoCode);
-};
-
-setCurrentVideoCode(); // Argument is required
-setCurrentVideoCode(""); //
-setCurrentVideoCode(null); // null
-setCurrentVideoCode("VD098"); // VD098
+console.log("Hello World")
