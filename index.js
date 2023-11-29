bdd6458f5699132f18b9097eb0089a17ecd58fa5
demo <demo@gmail.com>
2023-11-29 23:57:43 +0600

    variable, function, and class add

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-// let's do some interesting stuff with JS 
+// let's do some interesting stuff with JS
+// 1. let's create a variable
+const name = "John Doe";
+// 2. let's create a function
+function sayHello(name) {
+  console.log(`Hello ${name}`);
+}
 
-console.log("Hello World")
+sayHello(name);
+// 3. let's create a class
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.name}`);
+  }
+}
+
+const person = new Person(name);
+
+person.sayHello();
